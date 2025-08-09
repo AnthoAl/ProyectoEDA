1afc7fd2952b3bd4ed593c9542f8d55d675450b5
xNicki <166101049+xNicki@users.noreply.github.com>
2025-08-09 10:09:17 -0500

    Update NodoArbolB.java

--- ArbolAVL/src/ArbolB/NodoArbolB.java
+++ ArbolAVL/src/ArbolB/NodoArbolB.java
@@ -1,9 +1,12 @@
 package ArbolB;
 
-/**
- *
- * @author Carlos San Juan <carlossanjuanc@gmail.com>
+/*
+  Clase de un nodo de un Árbol B.
+  Un Árbol B es una estructura de datos de búsqueda balanceada,
+  utilizada comúnmente en bases de datos y sistemas de archivos
+  por su eficiencia al manejar grandes volúmenes de datos.
  */
+
 public class NodoArbolB {
 
     int n; //numero de claves almacenadas en el nodo
@@ -13,30 +16,38 @@ public class NodoArbolB {
 
     //Constructores
     public NodoArbolB(int t) {
-        n = 0;
-        leaf = true;
-        key = new int[((2 * t) - 1)];
-        child = new NodoArbolB[(2 * t)];
+        n = 0; // Inicialmente no hay claves
+        leaf = true; // Por defecto, el nodo es hoja
+        key = new int[((2 * t) - 1)]; // Espacio para el número máximo de claves
+        child = new NodoArbolB[(2 * t)]; // Espacio para el número máximo de hijos
     }
 
+
+    /*
+      Imprime el contenido del nodo en formato:
+      [clave1 | clave2 | ... | claveN]
+      Si es el último elemento, no coloca separaciones.
+     */
     public void imprimir() {
         System.out.print("[");
         for (int i = 0; i < n; i++) {
             if (i < n - 1) {
-                System.out.print(key[i] + " | ");
+                System.out.print(key[i] + " | "); // Clave con separador
             } else {
-                System.out.print(key[i]);
+                System.out.print(key[i]); // Última clave sin separador
             }
         }
         System.out.print("]");
     }
 
+    
+//Busca una clave especifica dentro del nodo
     public int find(int k) {
         for (int i = 0; i < n; i++) {
             if (key[i] == k) {
-                return i;
+                return i;  // Si encuentra la clave, retorna su posición
             }
         }
-        return -1;
+        return -1; // No encontrada
     }
 }
