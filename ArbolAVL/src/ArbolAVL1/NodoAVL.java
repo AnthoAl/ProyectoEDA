0a8db07520bd58a087b9d24465d0d3dcdd23ad59
lindsayVal <lindman23@outlook.com>
2025-08-09 19:15:24 -0500

    Update NodoAVL.java

--- ArbolAVL/src/ArbolAVL1/NodoAVL.java
+++ ArbolAVL/src/ArbolAVL1/NodoAVL.java
@@ -1,13 +1,13 @@
 package ArbolAVL1;
 
 public class NodoAVL {
-	int valor;
-	NodoAVL derecho;
-	NodoAVL izquierdo;
-	int altura;//importante para posteriormente calcular el balance
+	int valor;//el valor entero que contiene el nodo
+	NodoAVL derecho;//puntero hijo derecho
+	NodoAVL izquierdo;//puntero hijo izquierdo
+	int altura;//se agrega la altura para posteriormente calcular el balance
 
 	public NodoAVL(int valor) {
-		this.valor = valor;
-		this.altura = 1;//se inicializa la altura del nodo con 1
+		this.valor = valor;//se inicializa el valor con el parametro del constructor
+		this.altura = 1;//se inicializa la altura del nodo con 1, por ser un nodo hoja
 	}
 }
